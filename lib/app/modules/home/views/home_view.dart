5d18422a82ab9d22fd5b6e9afe0363d6ffdb5cf9
Sally <d0350315@gmail.com>
2021-12-08 16:14:13 +0800

    頂部菜單列List UI

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,39 +1,44 @@
+import 'dart:core';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_twstock/utils/half_circle.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  final List<String> menuLeft = ['test1', 'test2', 'test3'];
+  final List<String> menuRight = ['test4', 'test5', 'test6'];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[800],
+        backgroundColor: Colors.grey[800],
         body: Stack(
-      children: [
-        SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                width: Get.width,
-                height: 70,
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: Get.width,
+                    height: 70,
+                  ),
+                  priceIndex().paddingSymmetric(vertical: 10, horizontal: 15),
+                ],
               ),
-              priceIndex().paddingSymmetric(vertical:10,horizontal: 15),
-            ],
-          ),
-        ),
-        topMenu(),
-      ],
-    ));
+            ),
+            topMenu(),
+          ],
+        ));
   }
 
-  Widget topMenu(){
+  Widget topMenu() {
     return Column(
       children: [
         Obx(
-              () => Container(
+          () => Container(
             width: Get.width,
             height: this.controller.menuHeight.value,
             color: Colors.black87,
+            child: menuList(),
           ),
         ),
         GestureDetector(
@@ -41,8 +46,7 @@ class HomeView extends GetView<HomeController> {
             controller.startPositioned = e.globalPosition.dy;
           },
           onVerticalDragUpdate: (DragUpdateDetails details) {
-            if (details.globalPosition.dy >=
-                controller.startPositioned!) {
+            if (details.globalPosition.dy >= controller.startPositioned!) {
               controller.menuHeight.value =
                   details.globalPosition.dy - controller.startPositioned!;
             }
@@ -56,13 +60,14 @@ class HomeView extends GetView<HomeController> {
               buildClipPath(),
               Center(
                   child: GetBuilder<HomeController>(
-                    builder: (controller)=>Icon(
-                      controller.isOpen.value ?Icons.arrow_drop_up :Icons.filter_list_sharp,
-                      color: Colors.white,
-                      size: controller.isOpen.value ? 30 : 20,
-                    ),
-                  )
-              ).paddingOnly(bottom: 3),
+                builder: (controller) => Icon(
+                  controller.isOpen.value
+                      ? Icons.arrow_drop_up
+                      : Icons.filter_list_sharp,
+                  color: Colors.white,
+                  size: controller.isOpen.value ? 30 : 20,
+                ),
+              )).paddingOnly(bottom: 3),
             ],
           ),
         ),
@@ -70,15 +75,67 @@ class HomeView extends GetView<HomeController> {
     );
   }
 
-  Widget priceIndex(){
+  Widget menuList() {
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: 50,
+            height: 200,
+            color: Colors.blue,
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: menuLeft.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        menuLeft[index],
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ).paddingOnly(top: 15),
+                  );
+                }),
+          ),
+          SizedBox(
+            width: 50,
+          ),
+          Container(
+            width: 50,
+            height: 200,
+            color: Colors.red,
+            child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: menuRight.length,
+                itemBuilder: (context, index) {
+                  return Container(
+                    child: GestureDetector(
+                      onTap: () {},
+                      child: Text(
+                        menuRight[index],
+                        style: TextStyle(color: Colors.white, fontSize: 15),
+                      ),
+                    ).paddingOnly(top: 15),
+                  );
+                }),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget priceIndex() {
     return Card(
       color: Colors.grey[200],
       elevation: 15.0,
-      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(14.0))),
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(14.0))),
       child: Column(
         children: [
           Container(
-            width:double.maxFinite,
+            width: double.maxFinite,
             height: 120,
             decoration: BoxDecoration(
               color: Colors.blue,
@@ -89,7 +146,7 @@ class HomeView extends GetView<HomeController> {
                     offset: Offset(3.0, 3.0), //陰影y軸偏移量
                     blurRadius: 5, //陰影模糊程度
                     spreadRadius: 1 //陰影擴散程度
-                )
+                    )
               ],
             ),
             child: Row(
@@ -99,7 +156,7 @@ class HomeView extends GetView<HomeController> {
                 Text('17693.43'),
               ],
             ),
-          ).paddingSymmetric(horizontal: 10,vertical: 10),
+          ).paddingSymmetric(horizontal: 10, vertical: 10),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
@@ -113,10 +170,10 @@ class HomeView extends GetView<HomeController> {
     );
   }
 
-  Widget indexItem(){
+  Widget indexItem() {
     return Container(
-      width: Get.width*0.25,
-      height: Get.width*0.25,
+      width: Get.width * 0.25,
+      height: Get.width * 0.25,
       decoration: BoxDecoration(
         color: Colors.blue,
         borderRadius: BorderRadius.circular(8),
@@ -126,7 +183,7 @@ class HomeView extends GetView<HomeController> {
               offset: Offset(3.0, 3.0), //陰影y軸偏移量
               blurRadius: 5, //陰影模糊程度
               spreadRadius: 1 //陰影擴散程度
-          )
+              )
         ],
       ),
       child: Column(
@@ -138,6 +195,6 @@ class HomeView extends GetView<HomeController> {
           Text('9000.43'),
         ],
       ),
-    ).paddingSymmetric(vertical: 10,horizontal: 5);
+    ).paddingSymmetric(vertical: 10, horizontal: 5);
   }
 }
